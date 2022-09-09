bfea78054a1a14ccce4dd7c71bb9cf78b314c9af
Juscelino Cordeiro <jfilhodev@gmail.com>
2022-09-09 17:27:17 -0300

    wip

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,21 +1,28 @@
 <?php
 
-namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
+it('teste de home page')->get('/')
+->assertOk();
 
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_the_application_returns_a_successful_response()
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
-    }
-}
+
+
+// namespace Tests\Feature;
+
+// // use Illuminate\Foundation\Testing\RefreshDatabase;
+// use Tests\TestCase;
+
+// class ExampleTest extends TestCase
+// {
+//     /**
+//      * A basic test example.
+//      *
+//      * @return void
+//      */
+//     public function test_the_application_returns_a_successful_response()
+//     {
+//         $response = $this->get('/');
+
+//         $response->assertStatus(200);
+//     }
+// }
